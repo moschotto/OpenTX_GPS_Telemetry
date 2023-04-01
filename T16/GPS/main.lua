f1a500c99d77f7e917e701d82eb001f4339b66d0
moschotto <moschotto@gmx.de>
2023-04-01 15:26:47 +0200

    Add files via upload

--- T16/GPS/main.lua
+++ T16/GPS/main.lua
@@ -4,7 +4,7 @@ Copyright (C) by mosch
 License GPLv2: http://www.gnu.org/licenses/gpl-2.0.html       
 GITHUB: https://github.com/moschotto?tab=repositories 
 
-"TELEMETRY screen - GPS last known postions v2.4"  
+"TELEMETRY screen - GPS last known postions v2.5"  
 
  
 Description:
@@ -20,9 +20,6 @@ GPS coordinates and type it into to your mobile phone...
 
 - distance to your home position - how far you need to walk ;)
 
-- Reset telemetry DOES NOT WORK in HOROUS/JUMPER - event not implemented yet
-  Turn off/on to reset telem. data
-
 Install:
 - copy the GPS folder (inculding subfolders) to /WIDGETS/GPS
 - Setup 1/2 widget screen and select "GPS"
@@ -31,6 +28,7 @@ Install:
 
 
 log_filename = "/LOGS/GPSpositions.txt"
+local background_img
 local sat_img 
 local dis_img 
 local disT_img 
@@ -38,6 +36,7 @@ local home_img
 local drone_img
 local newtext_color = BLACK
 local newline_color = WHITE
+
 
 --[	####################################################################
 --[	functions
@@ -158,7 +157,8 @@ local newline_color = WHITE
 --############################################################################
 local options = {
   { "TextColor", COLOR, BLACK },
-  { "LineColor", COLOR, WHITE }
+  { "LineColor", COLOR, WHITE },
+  { "Debug", BOOL, 0 }
 }
 
 local function create(zone, options)
@@ -185,6 +185,7 @@ local function create(zone, options)
 	log_write_wait_time = 10,
 	old_time_write = 0,
 	update = true,
+	reset_home = false,
 	string_gmatch = string.gmatch,
 	now = 0,
 	ctr = 0,
@@ -205,6 +206,7 @@ local function create(zone, options)
 	disT_img = Bitmap.open("/WIDGETS/GPS/BMP/distanceT128.png")
 	home_img = Bitmap.open("/WIDGETS/GPS/BMP/home128.png")
 	drone_img = Bitmap.open("/WIDGETS/GPS/BMP/drone128.png")
+	background_img = Bitmap.open("/WIDGETS/GPS/BMP/background.png")
 	
 		
 	return wgt
@@ -238,10 +240,10 @@ local function get_data(wgt)
 	
 	--get IDs GPS Speed and GPS altitude
 	wgt.gpsspeedId = getTelemetryId("GSpd") --GPS ground speed m/s
-	wgt.gpsaltId = getTelemetryId("GAlt") --GPS altitude m
+	wgt.gpsaltId = getTelemetryId("Alt") --GPS altitude m
 	
 	--if "ALT" can't be read, try to read "GAlt"
-	if (gpsaltId == -1) then gpsaltId = getTelemetryId("GAlt") end	
+	if (wgt.gpsaltId == -1) then gpsaltId = getTelemetryId("GAlt") end	
 	
 	--if Stats can't be read, try to read Tmp2 (number of satellites SBUS/FRSKY)
 	if (wgt.gpssatId == -1) then wgt.gpssatId = getTelemetryId("Tmp2") end	
@@ -258,13 +260,13 @@ local function get_data(wgt)
 		wgt.gpsSpeed = rnd(getValue(wgt.gpsspeedId) * 1.852,1)
 		wgt.gpsALT = rnd(getValue(wgt.gpsaltId),0)
 		
-			
-		
+					
 		--set home postion only if more than 5 sats available
-		if (tonumber(wgt.gpsSATS) > 5) then
-			wgt.gpsLAT_H = rnd(wgt.gpsLatLon["pilot-lat"],6)
-			wgt.gpsLON_H = rnd(wgt.gpsLatLon["pilot-lon"],6)	
-		end
+		if (tonumber(wgt.gpsSATS) > 5) and (wgt.reset_home == true) then
+			wgt.gpsLAT_H = rnd(wgt.gpsLatLon["lat"],6)
+			wgt.gpsLON_H = rnd(wgt.gpsLatLon["lon"],6)	
+			wgt.reset_home = false			
+		end		
 
 		wgt.update = true	
 	else
@@ -302,18 +304,22 @@ local function get_data(wgt)
 	--####################################################################			
 	if (tonumber(wgt.gpsSATS) >= 5) then
 	
-		if (wgt.gpsLAT ~= wgt.gpsPrevLAT) and (wgt.gpsLON ~=  wgt.gpsPrevLON) and (wgt.gpsLAT_H ~= 0) and  (wgt.gpsLON_H ~= 0) then		
+		if (wgt.gpsLAT ~= wgt.gpsPrevLAT) and (wgt.gpsLON ~=  wgt.gpsPrevLON) then		
 		--if (string.len(gpsLAT) > 4) and  (string.len(gpsLON) > 4) and (tonumber(gpsSATS) > 5) then  
 					
 			--distance to home
-			wgt.gpsDtH = rnd(calc_Distance(wgt.gpsLAT, wgt.gpsLON, wgt.gpsLAT_H, wgt.gpsLON_H),2)			
-			wgt.gpsDtH = string.format("%.2f",wgt.gpsDtH)		
+			if (wgt.gpsLAT_H ~= 0) and  (wgt.gpsLON_H ~= 0) then
 			
-			--total distance traveled					
-			if (wgt.gpsPrevLAT ~= 0) and  (wgt.gpsPrevLON ~= 0) then	
+				wgt.gpsDtH = rnd(calc_Distance(wgt.gpsLAT, wgt.gpsLON, wgt.gpsLAT_H, wgt.gpsLON_H),2)			
+				wgt.gpsDtH = string.format("%.2f",wgt.gpsDtH)		
 				
-				wgt.gpsTotalDist =  rnd(tonumber(wgt.gpsTotalDist) + calc_Distance(wgt.gpsLAT,wgt.gpsLON,wgt.gpsPrevLAT,wgt.gpsPrevLON),2)			
-				wgt.gpsTotalDist = string.format("%.2f",wgt.gpsTotalDist)					
+				--total distance traveled					
+				if (wgt.gpsPrevLAT ~= 0) and  (wgt.gpsPrevLON ~= 0) then	
+					
+					wgt.gpsTotalDist =  rnd(tonumber(wgt.gpsTotalDist) + calc_Distance(wgt.gpsLAT,wgt.gpsLON,wgt.gpsPrevLAT,wgt.gpsPrevLON),2)			
+					wgt.gpsTotalDist = string.format("%.2f",wgt.gpsTotalDist)					
+				end
+				
 			end
 
 			--data for displaying the 
@@ -343,6 +349,7 @@ function refresh(wgt, event, touchState)
 	newtext_color = wgt.options.TextColor
 	newline_color = wgt.options.LineColor	
 	
+	
 	get_data(wgt) 	
 
 	--workaround to reset telemetry data via global session timer
@@ -352,48 +359,89 @@ function refresh(wgt, event, touchState)
 		wgt.gpsTotalDist = 0
 		wgt.gpsLAT_H = 0
 		wgt.gpsLON_H = 0	
+		wgt.reset_home = true
 	end 	
 	
-	-- display T16: 480*272px / 1/2 Zone size: 220x152 
-	--headline
-	if wgt.update == true then					
-		lcl_drawFilledRectangle(wgt.zone.x + 0, wgt.zone.y + 0, 225, 25, lcd.RGB(0x9D, 0xD6, 0x00))						
-		lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 5,wgt.gpsFIX , LEFT + SMLSIZE ,  newtext_color)
-	elseif wgt.update == false then		
-		lcl_drawFilledRectangle(wgt.zone.x + 0, wgt.zone.y + 0,  225, 25, lcd.RGB(0xE6, 0x32, 24))		
-		lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 5, "no GPS data available", LEFT + SMLSIZE ,  newtext_color)		
-	end
+	if wgt.options.Debug == 0 then
 	
-	--satellites
-	lcd.drawBitmap(sat_img, wgt.zone.x, wgt.zone.y + 30, 35)
-	lcl_drawText(wgt.zone.x + 42,wgt.zone.y + 40, wgt.gpsSATS, LEFT + MIDSIZE ,  newtext_color)		
+		-- display T16: 480*272px / 1/2 Zone size: 220x152 
+		--headline
+		if wgt.update == true then					
+			lcl_drawFilledRectangle(wgt.zone.x + 0, wgt.zone.y + 0, 225, 25, lcd.RGB(0x9D, 0xD6, 0x00))						
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 5,wgt.gpsFIX , LEFT + SMLSIZE ,  newtext_color)
+		elseif wgt.update == false then		
+			lcl_drawFilledRectangle(wgt.zone.x + 0, wgt.zone.y + 0,  225, 25, lcd.RGB(0xE6, 0x32, 24))		
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 5, "no GPS data available", LEFT + SMLSIZE ,  newtext_color)		
+		end
+				
+		--satellites
+		lcd.drawBitmap(sat_img, wgt.zone.x, wgt.zone.y + 30, 35)
+		lcl_drawText(wgt.zone.x + 42,wgt.zone.y + 40, wgt.gpsSATS, LEFT + MIDSIZE ,  newtext_color)		
+			
+		--distance to home
+		lcd.drawBitmap(dis_img, wgt.zone.x + 80, wgt.zone.y + 30, 35)
+		lcl_drawText(wgt.zone.x + 145 , wgt.zone.y + 30, wgt.gpsDtH, RIGHT + SMLSIZE ,  newtext_color)	
+		lcl_drawText(wgt.zone.x + 145 , wgt.zone.y + 50, "km", RIGHT + SMLSIZE ,  newtext_color)	
+			
+		--total total travel 
+		lcd.drawBitmap(disT_img, wgt.zone.x + 155, wgt.zone.y + 30, 35)
+		lcl_drawText(wgt.zone.x + 222, wgt.zone.y +30, wgt.gpsTotalDist, RIGHT + SMLSIZE ,  newtext_color)	
+		lcl_drawText(wgt.zone.x + 222, wgt.zone.y +50, "km", RIGHT + SMLSIZE ,  newtext_color)	
 		
-	--distance to home
-	lcd.drawBitmap(dis_img, wgt.zone.x + 80, wgt.zone.y + 30, 35)
-	lcl_drawText(wgt.zone.x + 145 , wgt.zone.y + 30, wgt.gpsDtH, RIGHT + SMLSIZE ,  newtext_color)	
-	lcl_drawText(wgt.zone.x + 145 , wgt.zone.y + 50, "km", RIGHT + SMLSIZE ,  newtext_color)	
-		
-	--total total travel 
-	lcd.drawBitmap(disT_img, wgt.zone.x + 155, wgt.zone.y + 30, 35)
-	lcl_drawText(wgt.zone.x + 222, wgt.zone.y +30, wgt.gpsTotalDist, RIGHT + SMLSIZE ,  newtext_color)	
-	lcl_drawText(wgt.zone.x + 222, wgt.zone.y +50, "km", RIGHT + SMLSIZE ,  newtext_color)	
-	
-	--line horz.
-	lcl_drawLine(wgt.zone.x + 0, wgt.zone.y + 85, wgt.zone.x + 224, wgt.zone.y + 85, SOLID, 0, newline_color)
+		--line horz.
+		lcl_drawLine(wgt.zone.x + 0, wgt.zone.y + 85, wgt.zone.x + 224, wgt.zone.y + 85, SOLID, 0, newline_color)
 
-	
-	--home location
-	lcd.drawBitmap(home_img, wgt.zone.x, wgt.zone.y + 95, 30)
-	lcl_drawText(wgt.zone.x + 50, wgt.zone.y + 110, wgt.gpsLAT_H .. ", " .. wgt.gpsLON_H, LEFT + SMLSIZE ,  newtext_color)	
 		
-	--line horz.
-	lcl_drawLine(wgt.zone.x + 0, wgt.zone.y + 145, wgt.zone.x + 224, wgt.zone.y + 145, SOLID, 0, newline_color)
+		--home location
+		lcd.drawBitmap(home_img, wgt.zone.x, wgt.zone.y + 95, 30)
+		
+		if (wgt.gpsLAT_H ~= 0) and  (wgt.gpsLON_H ~= 0) then
+			lcl_drawText(wgt.zone.x + 50, wgt.zone.y + 110, wgt.gpsLAT_H .. ", " .. wgt.gpsLON_H, LEFT + SMLSIZE,  newtext_color)	
+		else
+			lcl_drawText(wgt.zone.x + 50, wgt.zone.y + 90, "home not set" , LEFT + SMLSIZE + BLINK,  newtext_color)	
+			lcl_drawText(wgt.zone.x + 50, wgt.zone.y + 105, "reset STATS once a", LEFT + SMLSIZE ,  newtext_color)	
+			lcl_drawText(wgt.zone.x + 50, wgt.zone.y + 120, "GPS fix is obtained", LEFT + SMLSIZE ,  newtext_color)	
+		end
+			
+		--line horz.
+		lcl_drawLine(wgt.zone.x + 0, wgt.zone.y + 145, wgt.zone.x + 224, wgt.zone.y + 145, SOLID, 0, newline_color)
+		
+		--drone location (current and previous)
+		lcd.drawBitmap(drone_img, wgt.zone.x, wgt.zone.y + 150, 40)
+		lcl_drawText(wgt.zone.x + 50,wgt.zone.y + 158, wgt.coordinates_prev,LEFT + SMLSIZE ,  newtext_color)
+		lcl_drawText(wgt.zone.x + 50,wgt.zone.y + 180, wgt.coordinates_current,LEFT + SMLSIZE ,  newtext_color)
+			
 	
-	--current and last location
-	lcd.drawBitmap(drone_img, wgt.zone.x, wgt.zone.y + 150, 40)
-	lcl_drawText(wgt.zone.x + 50,wgt.zone.y + 158, wgt.coordinates_prev,LEFT + SMLSIZE ,  newtext_color)
-	lcl_drawText(wgt.zone.x + 50,wgt.zone.y + 180, wgt.coordinates_current,LEFT + SMLSIZE ,  newtext_color)
+	elseif wgt.options.Debug == 1 then
 	
+		if (type(wgt.gpsLatLon) == "table") then  
+									
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y,"Debug view:", LEFT + SMLSIZE ,  newtext_color)
+			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 15,"Lat: " .. rnd(wgt.gpsLatLon["lat"],6) , LEFT + SMLSIZE ,  newtext_color)			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 30,"Lon: " .. rnd(wgt.gpsLatLon["lon"],6) , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 45,"P lat: " .. rnd(wgt.gpsLatLon["pilot-lat"],6) , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 60,"P lon: " .. rnd(wgt.gpsLatLon["pilot-lon"],6) , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 75,"Home lat: " .. wgt.gpsLAT_H , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 90,"Home lon: " .. wgt.gpsLON_H , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 105,"Satellites: " .. wgt.gpsSATS , LEFT + SMLSIZE ,  newtext_color)			
+			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 120,"Dist home: " .. wgt.gpsDtH , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 120,wgt.zone.y + 120,"ALT: " .. wgt.gpsALT , LEFT + SMLSIZE ,  newtext_color)			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 135,"Dist travel: " .. wgt.gpsTotalDist , LEFT + SMLSIZE ,  newtext_color)			
+			lcl_drawText(wgt.zone.x + 120,wgt.zone.y + 135,"SPD: " .. wgt.gpsSpeed , LEFT + SMLSIZE ,  newtext_color)		
+			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 150,"GPS ID: " .. wgt.gpsId, LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 120,wgt.zone.y + 150,"SAT ID: " .. wgt.gpssatId, LEFT + SMLSIZE ,  newtext_color)			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 165,"SPD ID: " .. wgt.gpsspeedId , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 120,wgt.zone.y + 165,"ALT ID: " .. wgt.gpsaltId , LEFT + SMLSIZE ,  newtext_color)			
+			
+		else			
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 60,"No GPS data available" , LEFT + SMLSIZE ,  newtext_color)
+			lcl_drawText(wgt.zone.x + 5,wgt.zone.y + 75,"Please check the sensors" , LEFT + SMLSIZE ,  newtext_color)
+		end
+		
+	end
 	 
 end
 
